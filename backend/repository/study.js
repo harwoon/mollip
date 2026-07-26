dcdd131fef384fa3579c50b74e802feef293cd76
harwoon <dorothy8380@gmail.com>
2026-07-26 11:54:49 +0900

    반환 json 형식 수정

--- backend/repository/study.js
+++ backend/repository/study.js
@@ -3,5 +3,5 @@ import Study from "../models/Study.js"
 export async function createStudy(studyData) {
     const study = new Study(studyData)
     const savedStudy = await study.save()
-    return savedStudy._id.toString()
+    return savedStudy.toObject()
 }
