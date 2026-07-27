1c4aa9e8339490d435e4a96bf4c5f33ad4202395
harwoon <dorothy8380@gmail.com>
2026-07-27 17:15:10 +0900

    로그인/회원가입 UI 일부 완료

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import Login from "./pages/LoginPage"
 import Home from "./pages/HomePage"
+import Signup from "./pages/SignupPage"
 import Main from "./components/MainLayout"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Login />
+  },
+  {
+    path:"/signup",
+    element: <Signup/>
   },
   {
     // 2. 사이드바를 공유하는 페이지들 묶음
--- frontend/src/features/auth/api/auth.js
+++ frontend/src/features/auth/api/auth.js
@@ -1,7 +1,6 @@
+const API_URL = import.meta.env.VITE_LOCAL_API_URL
 
 export async function loginUser(userId, userPw) {
-
-    const API_URL = import.meta.env.VITE_LOCAL_API_URL
 
     const response = await fetch(`${API_URL}/auth/login`, {
         method: "POST",
@@ -11,4 +10,23 @@ export async function loginUser(userId, userPw) {
 
     const data = await response.json()
     if (!response.ok) throw new Error(data.message)
+}
+
+export async function signupUser(userId, userPw, nickname, email) {
+
+    const response = await fetch(`${API_URL}/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ userId, userPw, nickname, email }),
+    })
+
+    const data = await response.json()
+    if (!response.ok) throw new Error(data.message)
+}
+
+export async function checkIdUser(userId){
+    const response = await fetch(`${API_URL}/auth/checkId`,{
+        method: "GET",
+        body : JSON.stringify({userId})
+    })
 }
