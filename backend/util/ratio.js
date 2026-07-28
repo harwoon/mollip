e252475b1ca2c1b3003b597f2fd14343173abb20
배성욱 <qotjddnr1120@gmail.com>
2026-07-28 13:42:12 +0900

    과목별 비율 및 과목 색 가져오기

--- backend/util/ratio.js
+++ backend/util/ratio.js
@@ -1,37 +1,44 @@
+import { findColorByUserAndTitle } from "../repository/subject.js";
 // 사용자 과목별 비율 계산
-export function calculateStudyStatistics(studies) {
-    const subjectMap = new Map()
+export async function calculateStudyStatistics(studies) {
+  const subjectMap = new Map();
+  const userId = studies[0].user.toString();
 
-    for (const study of studies) {
-        const studyTitle = study.studyTitle?.trim() || "기타"
-        const studyTime = Number(study.sumStudyTime) || 0
+  for (const study of studies) {
+    const studyTitle = study.studyTitle?.trim() || "기타";
+    const studyTime = Number(study.sumStudyTime) || 0;
 
-        subjectMap.set(
-            studyTitle,
-            (subjectMap.get(studyTitle) || 0) + studyTime
-        )
-    }
+    subjectMap.set(studyTitle, (subjectMap.get(studyTitle) || 0) + studyTime);
+  }
 
-    const totalStudyTime = [...subjectMap.values()]
-        .reduce((total, studyTime) => total + studyTime, 0)
+  const totalStudyTime = [...subjectMap.values()].reduce(
+    (total, studyTime) => total + studyTime,
+    0,
+  );
 
-    const subjects = [...subjectMap.entries()]
-        .map(([studyTitle, sumStudyTime]) => ({
-            studyTitle,
-            sumStudyTime,
-            ratio:
-                totalStudyTime === 0
-                    ? 0
-                    : Number(
-                        (
-                            (sumStudyTime / totalStudyTime) * 100
-                        ).toFixed(2)
-                    ),
-        }))
-        .sort((a, b) => b.sumStudyTime - a.sumStudyTime)
+  const subjects = [...subjectMap.entries()]
+    .map(([studyTitle, sumStudyTime]) => ({
+      studyTitle,
+      sumStudyTime,
+      ratio:
+        totalStudyTime === 0
+          ? 0
+          : Number(((sumStudyTime / totalStudyTime) * 100).toFixed(2)),
+    }))
+    .sort((a, b) => b.sumStudyTime - a.sumStudyTime);
 
-    return {
-        totalStudyTime,
-        subjects,
-    }
+  
+  for (const subject of subjects) {
+    let color = "";
+    color = await findColorByUserAndTitle(userId, subject.studyTitle);
+    subject.subjectColor = color;
+  }
+  console.log("============subjects=============");
+  console.log(subjects);
+  
+
+  return {
+    totalStudyTime,
+    subjects,
+  };
 }
