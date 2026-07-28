e3d349f1785f4560e14d16c911956d351691b0b1
한혜원 <dorothy8380@gmail.com>
2026-07-28 15:17:19 +0900

    홈 달력 UI 추가

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,9 +1,13 @@
 import TodoList from "../features/home/components/TodoList"
+import HomeCalendar from "../features/home/components/HomeCalendar"
 
 export default function HomePage() {
   return (
+    <>
     <div> 
       <TodoList />
     </div>
+    <HomeCalendar/>
+    </>
   )
 }
