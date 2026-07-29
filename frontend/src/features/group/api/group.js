eb55cca3675366be5d7010ec43b15ecdb3d190fa
한혜원 <dorothy8380@gmail.com>
2026-07-29 15:49:10 +0900

    상위/하위 그룹 UI 완료

--- frontend/src/features/group/api/group.js
+++ frontend/src/features/group/api/group.js
@@ -25,3 +25,55 @@ export async function getMyGroup() {
     
     return {groupName, groupTime}
 }
+
+// 상위 그룹 조회
+export async function getHigher() {
+
+    const token = localStorage.getItem("token")
+
+    const response = await fetch(`${API_URL}/group/higher`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const data = await response.json()
+
+    if(!response.ok){
+        throw new Error(
+            data.message || "상위 그룹 정보를 불러오지 못했습니다."
+        )
+    }
+
+    const groupName = data.groupName
+    const groupTime = data.groupTime
+    
+    return {groupName, groupTime}
+}
+
+// 하위 그룹 조회
+export async function getLower() {
+
+    const token = localStorage.getItem("token")
+
+    const response = await fetch(`${API_URL}/group/lower`, {
+        method: "GET",
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    })
+
+    const data = await response.json()
+
+    if(!response.ok){
+        throw new Error(
+            data.message || "하위 그룹 정보를 불러오지 못했습니다."
+        )
+    }
+
+    const groupName = data.groupName
+    const groupTime = data.groupTime
+    
+    return {groupName, groupTime}
+}
