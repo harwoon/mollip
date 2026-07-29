019da83f173f54bb7c1f3216f91f0f8d7b7cce8f
LeeSeoJin <dltjwls5669@naver.com>
2026-07-29 15:05:55 +0900

    frontend_관리자 페이지 기초 레이아웃 파일 위치 수정 및 사이드바 진행중

---
+++ frontend/src/admin/components/AdminSidebar.jsx
@@ -0,0 +1,48 @@
+import { NavLink } from "react-router-dom"
+
+import "../../components/Sidebar.css"
+import SidebarUserInfo from "../../components/SidebarUserInfo.jsx"
+
+export default function AdminSidebar() {
+    const activeStyle = ({ isActive }) => {
+        return {
+        textDecoration: "none",
+        fontWeight: isActive ? "bold" : "normal",
+        color: isActive ? "#007bff" : "#333",
+        padding: "10px",
+        borderRadius: "8px",
+        backgroundColor: isActive ? "#e9ecef" : "transparent",
+        display: "block",
+        }
+    }
+
+    return (
+        <aside style={{ 
+        width: "250px", 
+        height: "100%", 
+        backgroundColor: "#f8f9fa", 
+        padding: "20px",
+        borderRight: "1px solid #ddd"
+        }}>
+            <div className="sidebarLogo">
+                <img src="/images/logo.png" alt="Mollip" />
+            </div>
+            <SidebarUserInfo /> {/* 확인하기 */}
+
+            <nav className="sidebarNavigation">
+                <NavLink to="/admin/home" style={activeStyle}>
+                    홈
+                </NavLink>
+
+                <NavLink to="/admin/users" style={activeStyle}>
+                    회원 조회
+                </NavLink>
+
+                <NavLink to="/admin/groups" style={activeStyle}>
+                    그룹 관리
+                </NavLink>
+            </nav>
+            
+        </aside>
+    )
+}
