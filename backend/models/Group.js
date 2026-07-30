070a07e55a36d3646e04c62845820dfa6818b56c
배성욱 <qotjddnr1120@gmail.com>
2026-07-30 20:43:25 +0900

    어드민 그룹관리 생성 수정

--- backend/models/Group.js
+++ backend/models/Group.js
@@ -1,5 +1,43 @@
 import mongoose from "mongoose"
 
+const groupGoalSchema = new mongoose.Schema(
+    {
+        goalType: {
+            type: String,
+            required: true,
+            enum: [
+                "MIN_STUDY_TIME",
+                "CHALLENGE_STUDY_TIME",
+                "TODO_COMPLETION_RATE",
+                "ATTENDANCE_DAYS"
+            ]
+        },
+        targetValue: {
+            type: Number,
+            required: true,
+            min: 0
+        },
+        unit: {
+            type: String,
+            required: true,
+            enum: [
+                "HOUR",
+                "PERCENT",
+                "DAY"
+            ]
+        },
+        order: {
+            type: Number,
+            required: true,
+            min: 1,
+            max: 4
+        }
+    },
+    {
+        _id: false
+    }
+)
+
 const groupSchema = new mongoose.Schema(
     {
         groupName: {
@@ -17,6 +55,10 @@ const groupSchema = new mongoose.Schema(
             required: true,
             min: 0,
             unique: true
+        },
+        goals: {
+            type: [groupGoalSchema],
+            default: []
         }
     },
     {
