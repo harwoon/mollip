d3ad70470c384e3cdd88e4f81d150ca3b5fb2e31
오승아 <sdesign416@naver.com>
2026-07-31 14:51:25 +0900

    back, front - 관리자홈-사용자주간 Todo달성률 2차테스트 완료

--- frontend/src/admin/features/home/api/user.js
+++ frontend/src/admin/features/home/api/user.js
@@ -1,3 +1,5 @@
+// import axios from "axios"
+
 const API_URL = import.meta.env.VITE_LOCAL_API_URL
 
 // 관리자 - 전체 사용자 수 조회
@@ -15,6 +17,32 @@ export async function getUserCount() {
 
     if(!response.ok) {
         throw new Error(data.message || "전체 사용자 수를 불러오지 못했습니다.")
+    }
+
+    return data
+}
+
+
+// 전체 사용자 주간 Todo 달성률
+export async function getWeeklyTodoAchievement() {
+    const token = localStorage.getItem("token")
+
+    const response = await fetch(
+        `${API_URL}/admin/todo-achievement/weekly`,
+        {
+            method: "GET",
+            headers: {
+                Authorization: `Bearer ${token}`
+            }
+        }
+    )
+
+    const data = await response.json()
+
+    if (!response.ok) {
+        throw new Error(
+            data.message || "전체 사용자 주간 Todo 달성률을 불러오지 못했습니다."
+        )
     }
 
     return data
