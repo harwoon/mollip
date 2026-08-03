7d4b1ee590c94c2811bb67c74268d044c5fbc0e9
한혜원 <dorothy8380@gmail.com>
2026-08-03 17:47:21 +0900

    url 수정

--- backend/router/ai.js
+++ backend/router/ai.js
@@ -5,7 +5,7 @@ import { isAuth } from "../middleware/auth.js"
 const router = express.Router()
 
 // gpt api 호출
-// GET http://localhost:3000/ai/weekly-report
+// GET http://127.0.0.1:3000/ai/weekly-report
 router.get("/weekly-report", isAuth, getWeeklyAiReport)
 
 export default router
