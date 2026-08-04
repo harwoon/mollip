7dae44ac0a75a36ed65db0579bbe4d072010819d
한혜원 <dorothy8380@gmail.com>
2026-08-04 17:22:55 +0900

    총 공부시간 대비

--- frontend/src/features/record/components/TotalStudy.jsx
+++ frontend/src/features/record/components/TotalStudy.jsx
@@ -9,6 +9,12 @@ export default function TotalStudy({ selectedDate, type }) {
   const [hour, setHour] = useState(0);
   const [min, setMin] = useState(0);
 
+  const [diffHour, setDiffHour] = useState(0)
+  const [diffMin, setDiffMin] = useState(0)
+
+  const [isDecrease, setIsDecrease] = useState(false)
+  const [isSame, setIsSame] = useState(true)
+
   useEffect(() => {
     const fetchStudyTime = async () => {
       try {
@@ -17,10 +23,20 @@ export default function TotalStudy({ selectedDate, type }) {
         // API 요청
         const data = await getStudyRecord(type, formattedDate);
 
-        const totalMinutes = data || 0;
+        const totalMinutes = data.current || 0
+        const diff = data.diff
+
+        const absDiff = Math.abs(diff)
 
         setHour(Math.floor(totalMinutes / 3600));
         setMin(Math.floor((totalMinutes % 3600) / 60));
+
+        setDiffHour(Math.floor(diff / 3600))
+        setDiffMin(Math.floor((diff % 3600) / 60))
+
+        setIsDecrease(data.diff < 0)
+        setIsSame(diff === 0)
+
       } catch (error) {
         console.error("공부 시간을 가져오는데 실패했습니다:", error);
       }
@@ -28,6 +44,8 @@ export default function TotalStudy({ selectedDate, type }) {
 
     fetchStudyTime();
   }, [selectedDate, type]);
+
+  const compareText = type === "daily" ? "어제" : type === "weekly" ? "저번 주" : "저번 달"
 
   return (
     <section className={`commonSection ${styles.container}`}>
@@ -41,6 +59,23 @@ export default function TotalStudy({ selectedDate, type }) {
 
           <p className={styles.description}>선택한 기간의 누적 학습시간</p>
         </div>
+      </div>
+
+      <div>
+        <span>{compareText}보다 </span>
+
+        {diffHour > 0 && (
+          <span>{String(diffHour).padStart(2, "0")}시간 </span>
+        )}
+        <span>{String(diffMin).padStart(2, "0")}분 </span>
+
+        {isSame ? (
+          <span>-</span>
+        ) : isDecrease ? (
+          <span>▼</span>
+        ) : (
+          <span>▲</span>
+        )}
       </div>
 
       <div className={styles.content}>
