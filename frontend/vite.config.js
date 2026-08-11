040943f3ce89c20f6259cf2680fd2f980dfd59e3
DK <kimlyu9212@gmail.com>
2026-08-11 16:52:53 +0900

    구글 프론트 헤더 추가 테스트용

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,12 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
   server: {
     // 외부 접속 허용
     host: true,
-    allowedHosts: true
-  }
+    allowedHosts: true,
+
+    // Google 로그인용 Referrer Policy
+    headers: {
+      'Referrer-Policy': 'strict-origin-when-cross-origin',
+    },
+  },
 })
